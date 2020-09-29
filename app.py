918b66e87f93eaf901bd4c4992f9b2c612473b91
Keerthana <60786451+Keerthanab14@users.noreply.github.com>
2020-09-29 14:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,11 @@ Pay_status = [
     "Payment delay for 8 month",
 ]
 
-limitbal = st.number_input('Limitbal')
-education = st.selectbox("Education ", ["Graduate school", "University", "High school", "others"])
+limitbal_value = st.number_input('Limitbal')
+education_index = st.selectbox("Education ", ["Graduate school", "University", "High school", "others"])
+edu = Education_inp.index(education) + 1
 marriage = st.selectbox("Marriage", ["Married", "Single", "Others"])
+marriage_index = Marriage_inp.index(marriage) + 1
 age = st.number_input('Age in years')
 payment_status = st.selectbox('Payment status', [
     "Account started that month with a zero balance, and never used any credit",
@@ -38,74 +40,56 @@ payment_status = st.selectbox('Payment status', [
     "Payment delay for 7 month",
     "Payment delay for 8 month",
 ]
-                              )
+ )
+status = Pay_status.index(payment_status) - 2
 
 st.subheader("Bill Amount for past 6 months")
-bill_month1 = st.number_input('Last month Bill amount (in NT dollar)')
-bill_month2 = st.number_input('Last 2nd month Bill amount (in NT dollar)')
-bill_month3 = st.number_input('Last 3rd month Bill amount (in NT dollar)')
-bill_month4 = st.number_input('Last 4th month Bill amount (in NT dollar)')
-bill_month5 = st.number_input('Last 5th month Bill amount (in NT dollar)')
-bill_month6 = st.number_input('Last 6th month Bill amount (in NT dollar)')
+bill_month1 = st.number_input('Last month Bill amount ')
+bill_month2 = st.number_input('Last 2nd month Bill amount ')
+bill_month3 = st.number_input('Last 3rd month Bill amount ')
+bill_month4 = st.number_input('Last 4th month Bill amount ')
+bill_month5 = st.number_input('Last 5th month Bill amount ')
+bill_month6 = st.number_input('Last 6th month Bill amount ')
 
 st.subheader("Paid Amount for past 6 months")
 paid_month1 = st.number_input('Amount paid in last month (in NT dollar)')
 paid_month2 = st.number_input('Amount paid in 2nd month (in NT dollar)')
 paid_month3 = st.number_input('Amount paid in 3rd month (in NT dollar)')
 paid_month4 = st.number_input('Amount paid in 4th month (in NT dollar)')
-paid_month5 = st.number_input('Amount paid in 5th month  (in NT dollar)')
+paid_month5 = st.number_input('Amount paid in 5th month (in NT dollar)')
 paid_month6 = st.number_input('amount paid in 6th month (in NT dollar)')
 
-
-edu = Education_inp.index(education) + 1
-mar = Marriage_inp.index(marriage) + 1
-stats = Pay_status.index(payment_status) - 2
-
-features = [int(limitbal), edu, mar, int(age), stats, int(bill_month1), int(bill_month2), int(bill_month3), int(bill_month4),
+features = [int(limitbal_value), education_index, marriage_index, int(age), status, int(bill_month1), int(bill_month2), int(bill_month3), int(bill_month4),
             int(bill_month5), int(bill_month6), int(paid_month1), int(paid_month2), int(paid_month3), int(paid_month4), int(paid_month5),
             int(paid_month6)]
 
 if st.button("Predict"):
     predicts = model.predict([features])
     output = round(predicts[0], 2)
-    prob = model.predict_proba([features])
-    y = prob[0]
+    probability = model.predict_proba([features])
+    y = probability[0]
     y = max(y)
     z = int(round(y * 100))
     if output == 1:
         st.warning("This account will be defaulted with a probability of {}%.".format(z))
     else:
-        st.success("This account might not be defaulted with a probability of {}%.".format(z))
+        st.success("This account will not be defaulted with a probability of {}%.".format(z))
         st.balloons()
 
 st.sidebar.header("About this App")
 st.sidebar.text(
     " This app predicts if the user will default \n the next month also based on the previous \n month payment and bill amount details")
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    
-    .sidebar .sidebar-content {
+st.markdown("""
+<style>
+body {
+    color: #111;
+    background-color:#AB9F38;
+}
+.sidebar .sidebar-content {
         background-image:
-        background-color:#8F8059;
+        background-color:#423D11;
         color: white;
     }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('bg.png')
+</style>
+    """, unsafe_allow_html=True)
