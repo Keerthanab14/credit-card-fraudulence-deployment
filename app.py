3d6005fe254c38508269b795cada4c6b3c08e2c4
Keerthana <60786451+Keerthanab14@users.noreply.github.com>
2020-09-29 13:47:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ page_bg_img = '''
     color: white;
     }
 body {
-  background-image: url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5e7df71b08eb080006520247%2F960x0.jpg%3Ffit%3Dscale&imgrefurl=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fleemathews%2F2020%2F03%2F27%2Fa-massive-credit-card-fraud-ring-just-got-shut-down-by-russian-authorities%2F&tbnid=Ya7qrCNmQt3ruM&vet=12ahUKEwj6xd2Iwo3sAhWaeH0KHV25ClUQMygKegUIARCzAQ..i&docid=sHA6cA4QKCGtMM&w=960&h=488&q=credit%20card%20fraud&ved=2ahUKEwj6xd2Iwo3sAhWaeH0KHV25ClUQMygKegUIARCzAQ#imgrc=Ya7qrCNmQt3ruM&imgdii=t9SLR7tDyW9PvM");
+  background-image: url("https://unsplash.com/photos/Q59HmzK38eQ");
   background-size: cover;
   background-opacity: 0.5;
 }
